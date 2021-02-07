8058c8919015843d8b6364fa8a97320d618cd26c
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-02-06 22:36:50 -0800

    creation de l'ecouter au serveur

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("chiza")
+//Import 
+
+var express =require('express');
+
+//instatiation du serveur
+var server=express();
+
+//routes
+server.get('/',function (req,res)  {
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send('<h1>Bonjour node</h1>');
+}); 
+
+//launcher seveur 
+
+server.listen(8080,function(){
+    console.log('Sever all ready :)');
+});
