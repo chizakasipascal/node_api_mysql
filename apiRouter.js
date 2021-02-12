9d37d7ce2cf46d53da8067a238e38b7e78838e32
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-02-12 15:05:18 -0800

    usercontrller

--- apiRouter.js
+++ apiRouter.js
@@ -2,9 +2,9 @@
 var express=require('express');
 var userController=require('./routes/userCtrl');
 
-//Routes
-exports.router=require(function () {
-    var apiRouter=express.Router();
+// Router
+exports.router = (function() {
+    var apiRouter = express.Router();
 
     //user Routes
     apiRouter.route('/users/register/').post(userController.register);
--- routes/userCtrl.js
+++ routes/userCtrl.js
@@ -1,11 +1,48 @@
 //import
 var bcrypt=require('body-parser');
 var jwt =require('jsonwebtoken');
+const { useInflection } = require('sequelize/types');
+var models =require('../models');
 
 //Routes
 module.exports={ //import de toutes nos root 
     register:function (req, res) {
-        
+        //params
+        var email=req.body.email;
+        var username=req.body.username;
+        var password=req.body.password;
+        var bio=req.body.bio;
+
+        if (email==null || username==null||password==null) {
+            return res.status(400).json({'erreur':'Parametre manquent'});
+        }
+
+        //TODO: Regex  verification 
+
+        models.User.findOne({
+            attributes:['email'],
+            where:{email:email}
+        }).the(function (userfound) {
+            if (!userfound) {
+                
+                    bcrypt.has(password,5,function (err,bcryptPassward) {
+                       var newUser=models.User.create(
+                           email=email,
+                           username=username,
+                           password=password,
+                           bio=bio,
+                           isAdmin=0
+
+                       ); 
+                    });
+
+            } else {
+                return res.status(409).json({'err':'Uttilisateur existe deja'});
+            }
+        }).catch(function (err) {
+            return err.status(500).json({'erro':'Invalide e-mail'}); 
+        });
+
     },
 
     login:function (req, res) {
